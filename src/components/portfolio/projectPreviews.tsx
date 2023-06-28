ad06dd0c63bde0d4c3c525c985982eb0b28c3ae6
Tyeetale <17817587+tyeetale@users.noreply.github.com>
2023-06-28 22:52:22 +0700

    add more succinct previews

--- src/components/portfolio/projectPreviews.tsx
+++ src/components/portfolio/projectPreviews.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { HiOutlineEye } from "react-icons/hi";
 import { AspectRatio } from "../ui/aspect-ratio";
 import { ScrollArea } from "../ui/scroll-area";
 
@@ -12,32 +13,50 @@ export const projects: {
     title: "Tildenn",
     href: "/portfolio/tildenn",
     previewImage: "/images/tildenn.png",
-    description: "The flexible travel app",
+    description: "the flexible travel app",
   },
   {
-    title: "Tildenn",
-    href: "/portfolio/tildenn",
-    previewImage: "/images/tildenn.png",
-    description: "The flexible travel app",
+    title: "Metaphor3d",
+    href: "/portfolio/metaphor3d",
+    previewImage: "/images/metaphor3d.png",
+    description: "ai 3d generated assets",
   },
   {
-    title: "Tildenn",
-    href: "/portfolio/tildenn",
-    previewImage: "/images/tildenn.png",
-    description: "The flexible travel app",
+    title: "GetFibes",
+    href: "/portfolio/getfibes",
+    previewImage: "/images/getfibes.png",
+    description: "influencer affiliate platform",
+  },
+  {
+    title: "Coopsight",
+    href: "/portfolio/coopsight",
+    previewImage: "/images/coopsight.png",
+    description: "startup ecosystem matchmaker",
+  },
+  {
+    title: "NYU Course Search",
+    href: "/portfolio/nyu-course-search",
+    previewImage: "/images/nyu-course-search.png",
+    description: "a better NYU class registrar",
+  },
+  {
+    title: "Deshihou",
+    href: "/portfolio/deshihou",
+    previewImage: "/images/deshihou.png",
+    description: "anime reminder app",
   },
 ];
 
 const ProjectPreviews = () => {
   return (
-    <section className="w-2/3 h-full">
+    <section className="w-2/3 h-full flex">
       <ScrollArea className="h-[calc(100vh-8rem)] w-full">
         <div className="flex flex-col space-y-6">
           {projects.map((project) => (
             <a
               key={project.title}
               href={project.href}
-              className="mx-auto w-full border-zinc-100 rounded-2xl relative border-2 overflow-hidden flex flex-col"
+              className="w-full border-zinc-100 dark:border-zinc-900 rounded-2xl relative border-2 overflow-hidden flex flex-col text-zinc-300 hover:text-zinc-900 hover:border-zinc-900"
             >
               <div className="relative w-full">
                 <AspectRatio ratio={16 / 9}>
@@ -48,7 +67,13 @@ const ProjectPreviews = () => {
                   />
                 </AspectRatio>
               </div>
-              <div className="h-max p-4 w-full">{project.title}</div>
+              <div className="h-max p-4 w-full flex justify-between items-center ">
+                <p className="font-bold text-lg ">{project.title}</p>
+                <div className="p-2 flex space-x-4 items-center">
+                  <HiOutlineEye size={24} className="stroke-[1.5px]" />
+                  <span className="">View</span>
+                </div>
+              </div>
             </a>
           ))}
         </div>
