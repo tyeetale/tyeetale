2878f17919ed91599882066b0990d10a7f3918b6
Tyeetale <tyeetale@gmail.com>
2024-04-21 14:39:31 +0700

    set boilerplate project
    new nextjs with yarn modern

--- src/utils/getAllProjects.ts
+++
@@ -1,8 +0,0 @@
-import { allProjects } from "contentlayer/generated";
-import { notFound } from "next/navigation";
-
-export async function getAllProjects() {
-  const projects = allProjects;
-  if (!projects) notFound(); // throws a 404 page
-  return projects.sort((a, b) => a.order - b.order);
-}
